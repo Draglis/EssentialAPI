3a5b34e90d1e5f989116b34b67c4fee3f71192a7
Draglis <68815080+Draglis@users.noreply.github.com>
2022-12-30 15:59:23 -0500

    add -plugin.yml and -Essential.php

---
+++ src/Draglis/EssentialAPI.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Draglis;
+
+use pocketmine\plugin\PluginBase;
+
+class EssentialAPI extends PluginBase {
+
+    protected function onEnable(): void {
+
+    }
+
+    protected function onDisable(): void {
+
+    }
+
+}
